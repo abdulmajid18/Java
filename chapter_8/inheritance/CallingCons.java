9040d83fc311deee0e457f56dafde0c4bd1d6290
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-11-30 14:12:09 +0000

    constructor-sequence

---
+++ chapter_8/inheritance/CallingCons.java
@@ -0,0 +1,25 @@
+package hello.chapter_8.inheritance;
+
+public class CallingCons {
+    public static void main(String[] args) {
+        C c = new C();
+    }
+}
+
+class A {
+    A() {
+        System.out.println("Inside A's constructor");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("Inside B's constructor.");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("Inside C's constructor");
+    }
+}
