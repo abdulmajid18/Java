c4236738524ab7aa2bca0ad3fbbf767e0c0368cd
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-06 09:48:57 +0000

    changes

--- chapter-3/scope.java
+++ chapter-3/scope.java
@@ -12,15 +12,15 @@ public class scope {
         System.out.println("x is " + x);
     }
 
-    class LifeTime {
-        public static void main(String[] args) {
-            int x;
-            for (x = 0; x < 3; x++) {
-                int y = -1; // y is initialized each time block is entered
-                System.out.println("y is: " + y); // this always prints -1
-                y = 100;
-                System.out.println("y is now: " + y);
-            }
-        }
-    }
-}
+// class LifeTime {
+// public static void gh(String[] args) {
+// int x;
+// for (x = 0; x < 3; x++) {
+// int y = -1; // y is initialized each time block is entered
+// System.out.println("y is: " + y); // this always prints -1
+// y = 100;
+// System.out.println("y is now: " + y);
+// }
+// }
+// }
+// }
