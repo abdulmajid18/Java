742b06b7daec9872bc5df41c2200ad7da65842af
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-28 13:35:40 +0000

    oop practice

--- fundamental/classes/CalenderTest/CalenderTest.java
+++ fundamental/classes/CalenderTest/CalenderTest.java
@@ -16,18 +16,17 @@ public class CalenderTest {
         int value = weekday.getValue();
 
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
-        for (int i = 1; i < value; i++) {
+        for (int i = 1; i < value; i++)
             System.out.print(" ");
-            while (date.getDayOfMonth() == month) {
-                System.out.printf("%3d", date.getDayOfMonth());
-                if (date.getDayOfMonth() == today)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-                date = date.plusDays(1);
-                if (date.getDayOfWeek().getValue() == 1)
-                    System.out.println();
-            }
+        while (date.getMonthValue() == month) {
+            System.out.printf("%3d", date.getDayOfMonth());
+            if (date.getDayOfMonth() == today)
+                System.out.print("*");
+            else
+                System.out.print(" ");
+            date = date.plusDays(1);
+            if (date.getDayOfWeek().getValue() == 1)
+                System.out.println();
         }
     }
 
