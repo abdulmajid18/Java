1df2a5fbbec7e19e78d174246e3356e48d4763c8
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-08 16:44:41 +0000

    static field

---
+++ fundamental/classes/StaticFields_StaticMethods/StaticField.java
@@ -0,0 +1,31 @@
+package hello.fundamental.classes.StaticFields_StaticMethods;
+
+public class StaticField {
+    public static void main(String[] args) {
+        Employee harry = new Employee("harry");
+        harry.use(harry);
+        Employee mark = new Employee("mark");
+        mark.use(mark);
+
+    }
+}
+
+class Employee {
+    private static int nextId = 0;
+    private int id;
+    String name;
+
+    Employee(String name) {
+        this.name = name;
+        id = nextId;
+        nextId++;
+    }
+
+    void use(Employee e) {
+        e.id = Employee.nextId;
+        Employee.nextId++;
+        System.out.println("Name of Employee: " + name);
+        System.out.println("ID:  " + e.id);
+        System.out.println("Next ID:  " + Employee.nextId);
+    }
+}
