c6ed093f3e8cb06e6c2f4300f848d3c2d3f7637b
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-08 14:39:46 +0000

    deadlock

---
+++ multithreading/interthread_process/Deadlock.java
@@ -0,0 +1,71 @@
+package hello.multithreading.interthread_process;
+
+public class Deadlock implements Runnable {
+    A a = new A();
+    B b = new B();
+    Thread t;
+
+    Deadlock() {
+        Thread.currentThread().setName("Main Thread");
+        t = new Thread(this, "Racing Thread");
+    }
+
+    void deadlockStart() {
+        t.start();
+        a.foo(b);
+        System.out.println("Back in main Thread");
+    }
+
+    public void run() {
+        b.bar(a);
+        System.out.println("Back in other Thred");
+    }
+
+    public static void main(String[] args) {
+        Deadlock d1 = new Deadlock();
+        d1.deadlockStart();
+    }
+
+}
+
+// An example of a Deadlock
+class A {
+    synchronized void foo(B b) {
+        String name = Thread.currentThread().getName();
+
+        System.out.println(name + " entered A.foo");
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("A Interrupted");
+        }
+
+        System.out.println(name + " trying to call B.last()");
+        b.last();
+    }
+
+    synchronized void last() {
+        System.out.println("Inside A.last");
+    }
+}
+
+class B {
+    synchronized void bar(A a) {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " entered B.bar");
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("B Interrupted");
+        }
+
+        System.out.println(name + " trying to call A.last()");
+        a.last();
+    }
+
+    synchronized void last() {
+        System.out.println("Inside A.last");
+    }
+}
