3a211abbd73f59b58eefae4d6dd17f14bf9d0ee6
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-09 18:04:19 +0000

    add: another instance

--- fundamental/classes/StaticFields_StaticMethods/StaticMethods.java
+++ fundamental/classes/StaticFields_StaticMethods/StaticMethods.java
@@ -2,8 +2,10 @@ package hello.fundamental.classes.StaticFields_StaticMethods;
 
 public class StaticMethods {
     public static void main(String[] args) {
-        int n = Employee.advanceId();
-        System.out.println("StaticMethods: " + n);
+        int n1 = Employee.advanceId();
+        System.out.println("StaticMethods 1: " + n1);
+        int n2 = Employee.advanceId();
+        System.out.println("StaticMethods 2: " + n2);
     }
 }
 
