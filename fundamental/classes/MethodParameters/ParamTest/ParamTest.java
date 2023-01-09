f007c6b1ded98760eeacbc77cfc6aaddbdcb3af7
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-09 21:05:17 +0000

    test: can't attach

--- fundamental/classes/MethodParameters/ParamTest/ParamTest.java
+++ fundamental/classes/MethodParameters/ParamTest/ParamTest.java
@@ -23,6 +23,18 @@ public class ParamTest {
         tripleSalary(harry);
         System.out.println("After: salary:" + harry.getName());
 
+        /*
+         * Test 3: Methods cant attach new objects to object
+         */
+        System.out.println("\nTesting swap");
+        var j = new Employee("John", 50000);
+        var d = new Employee("Doe", 60000);
+        System.out.println("Before: j=" + j.getName());
+        System.out.println("Before: d=" + d.getName());
+
+        swap(j, d);
+        System.out.println("After: j=" + j.getName());
+        System.out.println("After: d=" + d.getName());
     }
 
     public static void tripleValue(double x) {
