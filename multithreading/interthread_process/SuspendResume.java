6dda3005ef9d2bc310cb03b68fe3769f34ae720f
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-09 13:18:34 +0000

    suspend-resume action

---
+++ multithreading/interthread_process/SuspendResume.java
@@ -0,0 +1,46 @@
+package hello.multithreading.interthread_process;
+
+public class SuspendResume {
+
+}
+
+class NewThread implements Runnable {
+    String name;
+    Thread t;
+    boolean suspendFlag;
+
+    NewThread(String threadname) {
+        name = threadname;
+        t = new Thread(this, name);
+        System.out.println("New Thread: " + t);
+        suspendFlag = false;
+    }
+
+    // The entry point for Thread
+    public void run() {
+        try {
+            for (int i = 15; i > 0; i--) {
+                System.out.println(name + ": " + i);
+                Thread.sleep(200);
+                synchronized (this) {
+                    while (suspendFlag) {
+                        wait();
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println(name + " interrupted.");
+        }
+        System.out.println(name + " Exiting.");
+    }
+
+    synchronized void mysuspend() {
+        suspendFlag = true;
+    }
+
+    synchronized void myresume() {
+        suspendFlag = false;
+        notify();
+    }
+
+}
