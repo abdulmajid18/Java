fabe945a9d80c311170dbd0e1d8e8ef7af9e258d
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-10 14:46:25 +0000

    overloading

---
+++ fundamental/classes/ObjectConstruction/Overloading.java
@@ -0,0 +1,10 @@
+package hello.fundamental.classes.ObjectConstruction;
+
+public class Overloading {
+    public static void main(String[] args) {
+        var messages = new StringBuilder();
+        var todolist = new StringBuilder("To do:\n");
+        System.out.println("Message: " + messages);
+        System.out.println("Todo List: " + todolist);
+    }
+}
